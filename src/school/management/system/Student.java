e9513885f214b1cd20a05e38e0d3c6f898f24e34
Rich Hoyt <rhoyt10@gmail.com>
2022-04-11 09:31:26 -0400

    Add to student, add teacher class

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -4,6 +4,7 @@ package school.management.system;
  * and their name, grade, fees due, and fees paid.
  */
 public class Student {
+    // fields
     private int id;
     private String name;
     private int grade;
@@ -12,13 +13,29 @@ public class Student {
 
     /**
      * to create a new Student object by initializing values
+     * Fees for every student is going to be $30,000 per year.
+     * fees paid initially is $0.
      * @param studentId, id for the student: unique
      * @param studentName, name of the student
      * @param studentGrade, grade for the student
      */
     public Student(int studentId, String studentName, int studentGrade) {
         this.id = studentId;
-        this.name = studentName,
-        this.grade = studentGrade,
+        this.name = studentName;
+        this.grade = studentGrade;
+        this.feesTotal = 30_000;
+        this.feesPaid = 0;
+    }
+
+    /* We're not going to alter a student's name or id. */
+
+    // setters always void, getters return data types
+    // setGrade used to update student's grade @param grade is new grade
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void updateFeesPaid(int feesPaid) {
+        this.feesPaid += feesPaid;
     }
 }
