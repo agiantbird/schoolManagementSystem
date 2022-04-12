a4d37e31c3e691734666af0061f758658e8ee486
Rich Hoyt <rhoyt10@gmail.com>
2022-04-12 09:54:06 -0400

    Add to school class

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -1,4 +1,21 @@
 package school.management.system;
 
+import java.util.List;
+
 public class School {
+    /**
+     * School can have many teachers, many students
+     */
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent =0;
+    }
 }
